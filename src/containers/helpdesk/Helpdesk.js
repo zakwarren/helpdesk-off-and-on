f0ac12986da01fe5e88bc58ce697b86fab236983
zakwarren <41547855+zakwarren@users.noreply.github.com>
2020-12-25 14:46:23 +0000

    Options

--- src/containers/helpdesk/Helpdesk.js
+++ src/containers/helpdesk/Helpdesk.js
@@ -3,21 +3,39 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import * as actions from "../../store/actions";
-// import css from "./Helpdesk.module.css";
+import css from "./Helpdesk.module.css";
 import { Issue } from "../../components";
 
 export const Helpdesk = (props) => {
-  const { customer, issueType, issue, onGetCustomer } = props;
+  const { allOptions, customer, issueType, issue, onGetCustomer } = props;
 
   if (!customer || !issueType || !issue) {
     onGetCustomer();
     return null;
   }
 
-  return <Issue {...{ customer, issueType, issue }} />;
+  const shuffledOpts = allOptions[issueType].sort(() => 0.5 - Math.random());
+  const options = shuffledOpts.slice(0, 4);
+
+  return (
+    <>
+      <section className={css.IssueTray}>
+        <Issue {...{ customer, issueType, issue }} />
+      </section>
+      <section className={css.Controls}>
+        <h3>Options</h3>
+        <div className={css.OptionBtns}>
+          {options.map((opt, index) => (
+            <button key={index}>{opt}</button>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
 Helpdesk.propTypes = {
+  allOptions: PropTypes.object,
   customer: PropTypes.string,
   issueType: PropTypes.string,
   issue: PropTypes.string,
@@ -26,6 +44,7 @@ Helpdesk.propTypes = {
 
 export const mapStateToProps = (state) => {
   return {
+    allOptions: state.game.allOptions,
     customer: state.game.customer,
     issueType: state.game.issueType,
     issue: state.game.issue,
